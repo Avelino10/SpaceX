6091432d119e1fca3a11de1a9b48fa06bbb35678
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-09-18 14:26:01 +0100

    Make URLSesssionHTTPClient conform to the HTTPClient protocol

--- SpaceX/SpaceXTests/CompanyInfo API/URLSessionHTTPClientTests.swift
+++ SpaceX/SpaceXTests/CompanyInfo API/URLSessionHTTPClientTests.swift
@@ -8,7 +8,7 @@
 import SpaceX
 import XCTest
 
-class URLSessionHTTPClient {
+class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
 
     init(session: URLSession = .shared) {
@@ -89,7 +89,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 
     // MARK: - Helpers
 
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
