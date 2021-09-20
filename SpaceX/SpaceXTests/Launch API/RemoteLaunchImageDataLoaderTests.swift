eedd3c9352b41dc0bb197aceb41de1e5278f913c
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-09-20 15:47:14 +0100

    - `RemoteLaunchImageDataLoader` requests data from URL twice on calling `loadImageDataFromURL` twice - `RemoteLaunchImageDataLoader.loadImageDataFromURL` delivers error on client error

--- SpaceX/SpaceXTests/Launch API/RemoteLaunchImageDataLoaderTests.swift
+++ SpaceX/SpaceXTests/Launch API/RemoteLaunchImageDataLoaderTests.swift
@@ -14,8 +14,15 @@ class RemoteLaunchImageDataLoader {
         self.client = client
     }
 
-    func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
-        client.get(from: url) { _ in }
+    func loadImageData(from url: URL, completion: @escaping (LaunchImageDataLoader.Result) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+                case let .failure(error):
+                    completion(.failure(error))
+                default:
+                    break
+            }
+        }
     }
 }
 
@@ -35,6 +42,27 @@ class RemoteLaunchImageDataLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url])
     }
 
+    func test_loadImageDataFromURLTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.loadImageData(from: url) { _ in }
+        sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+
+    func test_loadImageDataFromURL_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        let clientError = NSError(domain: "a client error", code: 0)
+
+        expect(sut, toCompleteWith: .failure(clientError), when: {
+            client.complete(with: clientError)
+        })
+    }
+
+    // MARK: - Helpers
+
     private func makeSUT(url: URL = URL(string: "http://a-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteLaunchImageDataLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteLaunchImageDataLoader(client: client)
@@ -44,11 +72,41 @@ class RemoteLaunchImageDataLoaderTests: XCTestCase {
         return (sut, client)
     }
 
+    private func expect(_ sut: RemoteLaunchImageDataLoader, toCompleteWith expectedResult: LaunchImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let url = URL(string: "https://a-given-url.com")!
+        let exp = expectation(description: "Wait for load completion")
+
+        sut.loadImageData(from: url) { receivedResult in
+            switch (receivedResult, expectedResult) {
+                case let (.success(receivedData), .success(expectedData)):
+                    XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+                case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                    XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                default:
+                    XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+
+            exp.fulfill()
+        }
+
+        action()
+
+        wait(for: [exp], timeout: 1.0)
+    }
+
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
 
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-            requestedURLs.append(url)
+            messages.append((url, completion))
+        }
+
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
         }
     }
 }
